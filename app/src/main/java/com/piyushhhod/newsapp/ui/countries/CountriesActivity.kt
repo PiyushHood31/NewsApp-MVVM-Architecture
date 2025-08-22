94253e102f6c5f1c344553f8a52f88cbc80ceee1
PiyushHoood31 <piyushhood31@gmail.com>
2025-08-22 19:39:02 +0530

    add Countries List Screen to application

---
+++ app/src/main/java/com/piyushhhod/newsapp/ui/countries/CountriesActivity.kt
@@ -0,0 +1,86 @@
+package com.piyushhhod.newsapp.ui.countries
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.piyushhhod.newsapp.NewsApplication
+import com.piyushhhod.newsapp.R
+import com.piyushhhod.newsapp.databinding.ActivityCountriesBinding
+import com.piyushhhod.newsapp.di.component.DaggerActivityComponent
+import com.piyushhhod.newsapp.di.module.ActivityModule
+import com.piyushhhod.newsapp.ui.base.UiState
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class CountriesActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var countriesViewModel: CountriesViewModel
+
+    @Inject
+    lateinit var adapter :CountriesAdapter
+
+    private val tag = "CountriesActivity"
+    private lateinit var binding : ActivityCountriesBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        injectDependencies()
+        super.onCreate(savedInstanceState)
+        binding = ActivityCountriesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUI()
+        setObserver()
+
+    }
+
+    private fun setUI() {
+        val recyclerView = binding.countriesRecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+    private fun setObserver() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                countriesViewModel.uiState.collect{
+                    when(it){
+                        is UiState.Error -> {
+                            binding.countriesProgessBar.visibility = View.GONE
+                            Toast.makeText(this@CountriesActivity,it.message , Toast.LENGTH_LONG)
+                                .show()
+                        }
+                        UiState.Loading -> {
+                            binding.countriesProgessBar.visibility = View.VISIBLE
+                            binding.countriesRecyclerView.visibility = View.GONE
+                        }
+                        is UiState.Success -> {
+                            renderList(it.data)
+                            binding.countriesRecyclerView.visibility = View.VISIBLE
+                            binding.countriesProgessBar.visibility = View.GONE
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun renderList(list: List<String>) {
+        adapter.addData(list)
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun injectDependencies() {
+        DaggerActivityComponent.builder()
+            .applicationComponent((application as NewsApplication).applicationComponent)
+            .activityModule(ActivityModule(this))
+            .build()
+            .inject(this)
+    }
+}
---
+++ app/src/main/java/com/piyushhhod/newsapp/ui/countries/CountriesAdapter.kt
@@ -0,0 +1,46 @@
+package com.piyushhhod.newsapp.ui.countries
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.piyushhhod.newsapp.data.model.SourceX
+import com.piyushhhod.newsapp.databinding.SourceItemLayoutBinding
+
+class CountriesAdapter(
+    private val countriesList: ArrayList<String> = ArrayList()
+) : RecyclerView.Adapter<CountriesAdapter.DataViewHolder>() {
+    class DataViewHolder(private val binding: SourceItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(country: String) {
+            binding.sourceItem.text = country
+            itemView.setOnClickListener {
+                Log.d("CountriesAdapter", "Clicked on ${it}")
+            }
+        }
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder  =
+        DataViewHolder(
+            SourceItemLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount(): Int  = countriesList.size
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) =
+        holder.bind(countriesList[position])
+
+    fun addData(list : List<String>){
+        countriesList.addAll(list)
+    }
+
+
+}
